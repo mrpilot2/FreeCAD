5e04ded1b621a236844e5a635313e668fb503949
Markus Reitböck <mrpilot@gmx.net>
2025-08-30 16:24:50 +0200

    check if C4522 is still an issue
    multiple assignment operator specified

--- src/Mod/ReverseEngineering/App/PreCompiled.h
+++ src/Mod/ReverseEngineering/App/PreCompiled.h
@@ -28,7 +28,6 @@
 #ifdef _MSC_VER
 #pragma warning(disable : 4267)
 #pragma warning(disable : 4305)
-#pragma warning(disable : 4522)
 #endif
 
 // pcl headers include <boost/bind.hpp> instead of <boost/bind/bind.hpp>
