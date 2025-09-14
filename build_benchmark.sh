70c1d656a337ea0e41289649be533f4facf4f543
Markus Reitboeck <mrpilot@gmx.net>
2025-09-14 22:12:59 +0200

    ci: try new build approach
    only measure PCH performance of actual
    target, no dependencies

--- build_benchmark.sh
+++ build_benchmark.sh
@@ -1,5 +1,42 @@
+#!/bin/bash
+
+set -x
+
+delete_existing_target_build_dir() {
+    target=$1
+    
+    # remove just the target directory
+    base_target=${target//"Gui"/}
+
+    if [[ "$target" == "FreeCADBase" ]]
+    then
+        rm -rf $builddir/src/Base
+    elif [[ "$target" == "FreeCADApp" ]]
+    then
+        rm -rf $builddir/src/App
+    elif [[ "$target" == "FreeCADGui" ]]
+    then
+        rm -rf $builddir/src/Gui
+    elif [[ "$target" == *Gui ]]
+    then
+        rm -rf $builddir/src/Mod/$base_target/Gui
+    else
+        rm -rf $builddir/src/Mod/$base_target/App
+    fi
+}
+
+rm -rf $builddir/*
+
+pixi run configure-$config -DFREECAD_USE_PCH=OFF -DFREECAD_USE_CCACHE=OFF
+
 for target in $@
 do
+    # build once to make sure all dependencies are built
+
+    pixi run build-$config --target $target
+
+    delete_existing_target_build_dir $target
+
     for i in $(seq 1 3);
     do
         pixi run configure-$config -DFREECAD_USE_PCH=OFF -DFREECAD_USE_CCACHE=OFF
@@ -9,10 +46,21 @@ do
         echo "Building $target - PCH OFF" >> $logdir/build_timings.log
 
         (time pixi run build-$config --target $target) 2>> $logdir/build_timings.log
-    
-        rm -rf $builddir
-        mkdir -p $builddir
+   
+        if [[ $i -ne 3 ]]
+        then 
+            delete_existing_target_build_dir $target
+        fi
     done
+done
+
+pixi run configure-$config -DFREECAD_USE_PCH=ON -DFREECAD_USE_CCACHE=OFF
+for target in $@
+do
+    # build once to make sure all dependencies are built
+    pixi run build-$config --target $target
+
+    delete_existing_target_build_dir $target
 
     for i in $(seq 1 3);
     do
@@ -24,8 +72,10 @@ do
 
         (time pixi run build-$config --target $target) 2>> $logdir/build_timings.log
     
-        rm -rf $builddir
-        mkdir -p $builddir
+        if [[ $i -ne 3 ]]
+        then 
+            delete_existing_target_build_dir $target
+        fi
     done
 
 done
