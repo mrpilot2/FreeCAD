8ff7348eeca1a39dc6c05f6cb620e52e69944bc6
Markus Reitboeck <mrpilot@gmx.net>
2025-09-14 22:12:59 +0200

    ci: try new build approach
    only measure PCH performance of actual
    target, no dependencies

--- build_benchmark.sh
+++ build_benchmark.sh
@@ -1,5 +1,15 @@
 for target in $@
 do
+    # build once to make sure all dependencies are built
+    pixi run configure-$config -DFREECAD_USE_PCH=OFF -DFREECAD_USE_CCACHE=OFF
+
+    pixi run build-$config --target $target
+
+    # remove just the target directory
+    base_target=${target//"Gui"/}
+
+    rm -rf $builddir/src/Mod/$base_target
+
     for i in $(seq 1 3);
     do
         pixi run configure-$config -DFREECAD_USE_PCH=OFF -DFREECAD_USE_CCACHE=OFF
@@ -10,9 +20,18 @@ do
 
         (time pixi run build-$config --target $target) 2>> $logdir/build_timings.log
     
-        rm -rf $builddir
-        mkdir -p $builddir
+        rm -rf $builddir/src/Mod/$base_target
     done
+
+    # build once to make sure all dependencies are built
+    pixi run configure-$config -DFREECAD_USE_PCH=ON -DFREECAD_USE_CCACHE=OFF
+
+    pixi run build-$config --target $target
+
+    # remove just the target directory
+    base_target=${target//"Gui"/}
+
+    rm -rf $builddir/src/Mod/$base_target
 
     for i in $(seq 1 3);
     do
@@ -24,8 +43,7 @@ do
 
         (time pixi run build-$config --target $target) 2>> $logdir/build_timings.log
     
-        rm -rf $builddir
-        mkdir -p $builddir
+        rm -rf $builddir/src/Mod/$base_target
     done
 
 done
