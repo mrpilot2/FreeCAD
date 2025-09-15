2377b2791b55b2b35e5993429288d67d2dc5ceed
Markus Reitboeck <mrpilot@gmx.net>
2025-09-16 00:24:26 +0200

    Fem: use CMake to generate precompiled headers on all platforms
    "Professional CMake" book suggest the following:
    
    "Targets should build successfully with or without compiler support for precompiled headers. It
     should be considered an optimization, not a requirement. In particular, do not explicitly include a
     precompile header (e.g. stdafx.h) in the source code, let CMake force-include an automatically
     generated precompile header on the compiler command line instead. This is more portable across
     the major compilers and is likely to be easier to maintain. It will also avoid warnings being
     generated from certain code checking tools like iwyu (include what you use)."
    
    Therefore, removed the "#include <PreCompiled.h>" from sources, also
    there is no need for the "#ifdef _PreComp_" anymore

--- src/Mod/Fem/App/PreCompiled.h
+++ src/Mod/Fem/App/PreCompiled.h
@@ -25,8 +25,6 @@
 
 #include <FCConfig.h>
 
-#ifdef _PreComp_
-
 // standard
 #include <algorithm>
 #include <bitset>
@@ -101,6 +99,8 @@
 #include <StdMeshers_UseExisting_1D2D.hxx>
 
 // Opencascade
+#include <Standard_Version.hxx>
+
 #include <Adaptor3d_IsoCurve.hxx>
 #include <BRepAdaptor_CompCurve.hxx>
 #include <BRepAdaptor_Curve.hxx>
@@ -133,7 +133,6 @@
 #include <ShapeAnalysis_ShapeTolerance.hxx>
 #include <ShapeAnalysis_Surface.hxx>
 #include <Standard_Real.hxx>
-#include <Standard_Version.hxx>
 #include <TColgp_Array2OfPnt.hxx>
 #include <TopoDS.hxx>
 #include <TopoDS_Edge.hxx>
@@ -208,5 +207,4 @@
 #include <NETGENPlugin_SimpleHypothesis_3D.hxx>
 #endif
 
-#endif  // _PreComp_
 #endif
