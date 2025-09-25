8f7cd81b72b67313313de85efe42504f5a9d94b3
Markus Reitböck <mrpilot@gmx.net>
2025-09-26 00:22:04 +0200

    we need them?

--- src/Gui/PythonWrapper.cpp
+++ src/Gui/PythonWrapper.cpp
@@ -82,9 +82,6 @@
 // Do not use SHIBOKEN_MICRO_VERSION; it might contain a dot
 #  define SHIBOKEN_FULL_VERSION QT_VERSION_CHECK(SHIBOKEN_MAJOR_VERSION, SHIBOKEN_MINOR_VERSION, 0)
 
-#  include <pyside2_qtcore_python.h>
-#  include <pyside2_qtgui_python.h>
-#  include <pyside2_qtwidgets_python.h>
 # endif // HAVE_PYSIDE2
 #endif // HAVE_SHIBOKEN2
 
